9de1ed00f80fe2bd511339b6a8d528e50e5ddd83
NiveditJain <nivedit@aikin.club>
2025-07-07 13:33:36 +0530

    fixing linting issues

--- api-server/app/project/models/create_project_response.py
+++ api-server/app/project/models/create_project_response.py
@@ -1,11 +1,6 @@
 from pydantic import BaseModel
-from .project_database_model import Project
 from .billing_account import BillingAccount
-from .project_user import ProjectUser
 from .project_status_enum import ProjectStatusEnum
-from beanie import Link
-from typing import List
-from app.user.models.user_database_model import User
 from datetime import datetime
 
 
