d7024735ea1e08f7fb4c0ba236ef604f77e61317
NiveditJain <nivedit@aikin.club>
2025-07-13 17:44:00 +0530

    added model of satellite

--- api-server/app/satellite/models/satellite_database_model.py
+++ api-server/app/satellite/models/satellite_database_model.py
@@ -4,6 +4,7 @@ from beanie import Document, Indexed, before_event, Replace, Save, Link
 from datetime import datetime
 from pydantic import Field, Any, field_validator
 from .access_types import AccessTypeEnum
+from docker_image import reference
 
 from app.project.models.project_database_model import Project
 
@@ -29,6 +30,9 @@ class Satellite(Document):
 
     updated_at: datetime = Field(default_factory=datetime.now, description="Date and time when the satellite was last updated")
 
+    image_uri: str = Field(..., description="OCI/Docker image URI for the satellite")
+
+    timeout: int = Field(..., description="Timeout of the satellite in seconds")
 
     @field_validator("configs", "inputs", "metrics", "outputs")
     def validate_jsonschema(cls, v: dict[str, Any]) -> dict[str, Any]:
@@ -40,7 +44,14 @@ class Satellite(Document):
             raise ValueError(f"Invalid JSON schema: {e.message}")
 
         return v
-
+    
+    @field_validator("image_uri")
+    def validate_image_uri(cls, v: str) -> str:
+        try:
+            reference.Reference.parse(v)
+        except Exception as e:
+            raise ValueError(f"Invalid image URI: {e.message}")
+        return v
 
     @before_event([Save, Replace])
     def update_updated_at(self):
