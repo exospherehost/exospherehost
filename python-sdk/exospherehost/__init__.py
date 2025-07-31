aa06bb35e05f33d6af429ce182143fe8256f627a
NiveditJain <nivedit@aikin.club>
2025-07-31 14:32:14 +0530

    fixed ruff checks

--- python-sdk/exospherehost/__init__.py
+++ python-sdk/exospherehost/__init__.py
@@ -2,4 +2,6 @@ from ._version import version as __version__
 from .runtime import Runtime
 from .node.BaseNode import BaseNode
 
-__all__ = ["Runtime", "BaseNode"]
+VERSION = __version__
+
+__all__ = ["Runtime", "BaseNode", "VERSION"]
