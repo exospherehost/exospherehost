e023ee61bee5e3d4fbba79f0155a2611cf7239f7
NiveditJain <nivedit@aikin.club>
2025-08-03 19:55:33 +0530

    fixed spell check

--- state-manager/app/models/node_template_model.py
+++ state-manager/app/models/node_template_model.py
@@ -5,7 +5,7 @@ from typing import Any, Optional, List
 class NodeTemplate(BaseModel):
     name: str = Field(..., description="Name of the node")
     namespace: str = Field(..., description="Namespace of the node")
-    indentifier: str = Field(..., description="Indentifier of the node")
+    identifier: str = Field(..., description="Identifier of the node")
     inputs: dict[str, Any] = Field(..., description="Inputs of the node")
     store: dict[str, Any] = Field(..., description="Upsert data to store object for the node")
     next_nodes: Optional[List[str]] = Field(None, description="Next nodes to execute")
