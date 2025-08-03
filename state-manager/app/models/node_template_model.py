a897eea23c42bb16ce6046e9ea85955242fde939
NiveditJain <nivedit@aikin.club>
2025-08-03 20:06:49 +0530

    fixed comments by @coderabbitai

--- state-manager/app/models/node_template_model.py
+++ state-manager/app/models/node_template_model.py
@@ -3,7 +3,7 @@ from typing import Any, Optional, List
 
 
 class NodeTemplate(BaseModel):
-    name: str = Field(..., description="Name of the node")
+    node_name: str = Field(..., description="Name of the node")
     namespace: str = Field(..., description="Namespace of the node")
     identifier: str = Field(..., description="Identifier of the node")
     inputs: dict[str, Any] = Field(..., description="Inputs of the node")
