7e25e256237e94e7681a625ed1cb3683676f01bf
NiveditJain <nivedit@aikin.club>
2025-08-06 13:47:04 +0530

    creating api to create init state

--- state-manager/app/models/create_models.py
+++ state-manager/app/models/create_models.py
@@ -1,3 +1,4 @@
+from xml.dom.minidom import Identified
 from pydantic import BaseModel, Field
 from typing import Any
 from .state_status_enum import StateStatusEnum
@@ -5,14 +6,14 @@ from datetime import datetime
 
 
 class RequestStateModel(BaseModel):
-    node_name: str = Field(..., description="Name of the node of the state")
-    graph_name: str = Field(..., description="Name of the graph template for this state")
+    identifier: str = Field(..., description="Name of the node of the state")
     inputs: dict[str, Any] = Field(..., description="Inputs of the state")
 
 
 class ResponseStateModel(BaseModel):
     state_id: str = Field(..., description="ID of the state")
     node_name: str = Field(..., description="Name of the node of the state")
+    identifier: str = Field(..., description="Identifier of the node for which state is created")
     graph_name: str = Field(..., description="Name of the graph template for this state")
     inputs: dict[str, Any] = Field(..., description="Inputs of the state")
     created_at: datetime = Field(..., description="Date and time when the state was created")
