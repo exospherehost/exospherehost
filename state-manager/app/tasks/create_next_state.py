4bef3053f6e52002942c6f7a89d77809475cbca9
NiveditJain <nivedit@aikin.club>
2025-08-16 18:42:41 +0530

    fixed ruff

--- state-manager/app/tasks/create_next_state.py
+++ state-manager/app/tasks/create_next_state.py
@@ -6,7 +6,7 @@ from app.models.db.graph_template_model import GraphTemplate
 from app.models.graph_template_validation_status import GraphTemplateValidationStatus
 from app.models.db.registered_node import RegisteredNode
 from app.models.state_status_enum import StateStatusEnum
-from beanie.operators import NE, NotIn
+from beanie.operators import NE
 from app.singletons.logs_manager import LogsManager
 
 from json_schema_to_pydantic import create_model
