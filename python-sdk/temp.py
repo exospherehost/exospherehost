11edd0ff06602bde8e323ca73ac5d60c6c08919b
NiveditJain <nivedit@aikin.club>
2025-08-17 13:57:41 +0530

    removed temp file

--- python-sdk/temp.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-from exospherehost import StateManager, TriggerState
-
-stateManager = StateManager(namespace="test", state_manager_uri="http://localhost:8000", key="Mathrithms@2021")
-
-asyncio.run(stateManager.trigger("test", TriggerState(identifier="test", inputs={"test": "test"})))
