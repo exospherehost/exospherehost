ff53d5ea89d5ffec8a108d93c71b32665a50c081
NiveditJain <nivedit@aikin.club>
2025-08-28 23:43:46 +0530

    test if tests are working

--- state-manager/tests/unit/with_database/test_graph_template.py
+++ state-manager/tests/unit/with_database/test_graph_template.py
@@ -2,7 +2,7 @@ from app.models.db.graph_template_model import GraphTemplate
 from app.models.graph_template_validation_status import GraphTemplateValidationStatus
 from app.models.node_template_model import NodeTemplate
 
-async def test_graph_template(app_started):
+async def test_graph_template_basic(app_started):
     """Test graph template creation"""
     graph_template_model = GraphTemplate(
         name="test_graph_template",
@@ -20,3 +20,34 @@ async def test_graph_template(app_started):
         validation_status=GraphTemplateValidationStatus.PENDING,
     )
     assert graph_template_model.name == "test_graph_template"
+
+async def test_liner_graph_template(app_started):
+    """Test liner graph template creation"""
+    graph_template_model = GraphTemplate(
+        name="test_liner_graph_template",
+        namespace="test_namespace",
+        nodes=[
+            NodeTemplate(
+                node_name="node1",
+                namespace="test_namespace",
+                identifier="node1",
+                inputs={},
+                next_nodes=[
+                    "node2"
+                ],
+                unites=None
+            ),
+            NodeTemplate(
+                node_name="node2",
+                namespace="test_namespace",
+                identifier="node2",
+                inputs={},
+                next_nodes=None,
+                unites=None
+            )
+        ],
+        validation_status=GraphTemplateValidationStatus.PENDING
+    )
+    assert graph_template_model.get_root_node().identifier == "node1"
+    assert graph_template_model.get_parents_by_identifier("node1") == set()
+    assert graph_template_model.get_parents_by_identifier("node2") == {"node1"}
