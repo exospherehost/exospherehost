8c8fe28b5bd200fa8b97ff80fada1673875b745a
NiveditJain <nivedit@aikin.club>
2025-08-29 12:35:00 +0530

    minor fixes

--- state-manager/tests/unit/with_database/test_graph_template.py
+++ state-manager/tests/unit/with_database/test_graph_template.py
@@ -1,7 +1,10 @@
+import pytest
+
 from app.models.db.graph_template_model import GraphTemplate
 from app.models.graph_template_validation_status import GraphTemplateValidationStatus
 from app.models.node_template_model import NodeTemplate
 
+@pytest.mark.asyncio
 async def test_graph_template_basic(app_started):
     """Test graph template creation"""
     graph_template_model = GraphTemplate(
@@ -21,6 +24,7 @@ async def test_graph_template_basic(app_started):
     )
     assert graph_template_model.name == "test_graph_template"
 
+@pytest.mark.asyncio
 async def test_liner_graph_template(app_started):
     """Test liner graph template creation"""
     graph_template_model = GraphTemplate(
@@ -51,3 +55,17 @@ async def test_liner_graph_template(app_started):
     assert graph_template_model.get_root_node().identifier == "node1"
     assert graph_template_model.get_parents_by_identifier("node1") == set()
     assert graph_template_model.get_parents_by_identifier("node2") == {"node1"}
+    assert graph_template_model.get_node_by_identifier("node1").identifier == "node1" # type: ignore
+    assert graph_template_model.get_node_by_identifier("node2").identifier == "node2" # type: ignore
+
+
+@pytest.mark.asyncio
+async def test_graph_template_invalid_liner_graph_template(app_started):
+    """Test invalid liner graph template creation"""
+    with pytest.raises(ValueError, match="There should be exactly one root node in the graph but found 0 nodes with zero in-degree: \\[\\]"):
+        GraphTemplate(
+            name="test_invalid_liner_graph_template",
+            namespace="test_namespace",
+            nodes=[],
+            validation_status=GraphTemplateValidationStatus.PENDING
+        )
