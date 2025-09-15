28622b109a6b5b86dc9bc1dcfe6fe7ef36e55bfe
NiveditJain <nivedit@aikin.club>
2025-09-16 00:05:24 +0530

    fix: adjust padding in select component for improved layout
    - Updated the padding in the select component to enhance the overall layout and user experience.
    - This change ensures better alignment and spacing within the dropdown, contributing to a more polished interface.

--- dashboard/src/components/ui/select.tsx
+++ dashboard/src/components/ui/select.tsx
@@ -10,7 +10,7 @@ const Select = React.forwardRef<
     <div className="relative">
       <select
         className={cn(
-          "flex h-12 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-input text-foreground px-3 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 appearance-none [&>option]:bg-popover [&>option]:text-popover-foreground",
+          "flex h-12 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-input text-foreground px-3 pr-10 text-sm shadow-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 appearance-none [&>option]:bg-popover [&>option]:text-popover-foreground",
           className
         )}
         ref={ref}
