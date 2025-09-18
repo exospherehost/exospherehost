596a183fd54222fc50442093b4ff9ad31f248adb
NiveditJain <nivedit@aikin.club>
2025-09-18 16:04:11 +0530

    Enhance upsert_graph tests and update validation settings
    - Added a new test for valid and invalid graph upsert scenarios, ensuring proper validation status handling.
    - Updated the GraphTemplate model to enable validation on save, improving data integrity during database operations.
    - Removed validation on save from the State model to streamline the validation process.
    - Updated dependencies in the project, including cffi and ruff, to their latest versions for improved performance and security.

--- integration-tests/test_upsert_graph.py
+++ integration-tests/test_upsert_graph.py
@@ -52,3 +52,109 @@ async def test_upsert_graph(running_server):
     
     assert data is not None
     assert data["validation_status"] == "VALID"
+
+
+async def test_valid_invalid_valid_upsert_graph(running_server):
+    class Node1(BaseNode):
+        class Inputs(BaseModel):
+            message: str
+        
+        async def execute(self):
+            print(self.inputs.message) # type: ignore
+
+    class Node2(BaseNode):
+        class Inputs(BaseModel):
+            message: str
+        
+        async def execute(self):
+            print(self.inputs.message) # type: ignore
+
+    state_machine_url = running_server.base_url
+    runtime = Runtime(
+        namespace="test",
+        name="test",
+        nodes=[
+            Node1,
+            Node2
+        ],
+        state_manager_uri=state_machine_url,
+    )
+    
+    thread = threading.Thread(target=runtime.start, daemon=True)
+    thread.start()
+
+    await asyncio.sleep(2)
+
+    state_manager = StateManager(
+        namespace="test",
+        state_manager_uri=state_machine_url
+    )
+
+    data = await state_manager.upsert_graph(
+        graph_name="valid_graph",
+        graph_nodes=[
+            GraphNodeModel(
+                node_name="Node1",
+                namespace="test",
+                identifier="node1",
+                inputs={
+                    "message": "Hello, world!",
+                }
+            )
+        ],
+        secrets={},
+    )
+
+    assert data is not None
+    assert data["validation_status"] == "VALID"
+
+    with pytest.raises(Exception) as exec_info:
+        data = await state_manager.upsert_graph(
+            graph_name="valid_graph",
+            graph_nodes=[
+                GraphNodeModel(
+                    node_name="Node1",
+                    namespace="test",
+                    identifier="node1",
+                    inputs={
+                        "message": "Hello, world!",
+                    }
+                ),
+                GraphNodeModel(
+                    node_name="Node2",
+                    namespace="test",
+                    identifier="node2",
+                    inputs={
+                        "message": "Hello, world!",
+                    }
+                )
+            ],
+            secrets={},
+        )
+    print(exec_info.value)
+
+    data = await state_manager.upsert_graph(
+        graph_name="valid_graph",
+        graph_nodes=[
+            GraphNodeModel(
+                node_name="Node1",
+                namespace="test",
+                identifier="node1",
+                inputs={
+                    "message": "Hello, world!",
+                },
+                next_nodes=["node2"]
+            ),
+            GraphNodeModel(
+                node_name="Node2",
+                namespace="test",
+                identifier="node2",
+                inputs={
+                    "message": "Hello, world!",
+                }
+            )
+        ],
+        secrets={},
+    )
+    assert data is not None
+    assert data["validation_status"] == "VALID"
--- state-manager/app/models/db/state.py
+++ state-manager/app/models/db/state.py
@@ -63,7 +63,6 @@ class State(BaseDatabaseModel):
         return await super().insert_many(documents) # type: ignore
         
     class Settings:
-        validate_on_save = True
         indexes = [
             IndexModel(
                 [
