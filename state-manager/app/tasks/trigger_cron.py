9fdda88b75dacca4c56590c916627d0ba5475047
NiveditJain <nivedit@aikin.club>
2025-09-28 20:32:12 +0530

    Implement trigger cron logic for processing due triggers
    - Added functions to retrieve and update due triggers in the database, changing their status from PENDING to TRIGGERED.
    - Implemented the `call_trigger_graph` function to execute the trigger graph with a unique request ID.
    - Enhanced the `trigger_cron` function to log the start time and process due triggers in a loop, calling the necessary functions for each trigger.
    - Introduced a placeholder for `create_next_triggers` to facilitate future trigger creation logic.

--- state-manager/app/tasks/trigger_cron.py
+++ state-manager/app/tasks/trigger_cron.py
@@ -1,4 +1,42 @@
 from datetime import datetime
+from uuid import uuid4
+from app.models.db.trigger import DatabaseTriggers
+from app.models.trigger_models import TriggerStatusEnum
+from app.singletons.logs_manager import LogsManager
+from app.controller.trigger_graph import trigger_graph
+from app.models.trigger_graph_model import TriggerGraphRequestModel
+from pymongo import ReturnDocument
+
+logger = LogsManager().get_logger()
+
+async def get_due_triggers(cron_time: datetime) -> DatabaseTriggers | None:
+    data = await DatabaseTriggers.get_pymongo_collection().find_one_and_update(
+        {
+            "trigger_time": {"$lte": cron_time},
+            "trigger_status": TriggerStatusEnum.PENDING
+        },
+        {
+            "$set": {"trigger_status": TriggerStatusEnum.TRIGGERED}
+        },
+        return_document=ReturnDocument.AFTER
+    )
+    return DatabaseTriggers(**data) if data else None
+
+async def call_trigger_graph(trigger: DatabaseTriggers):
+    await trigger_graph(
+        namespace_name=trigger.namespace,
+        graph_name=trigger.graph_name,
+        body=TriggerGraphRequestModel(),
+        x_exosphere_request_id=str(uuid4())
+    )
+
+async def create_next_triggers(trigger: DatabaseTriggers):
+    pass
 
 async def trigger_cron():
-    print(f"From trigger_cron: {datetime.now()}")
+    cron_time = datetime.now()
+    logger.info(f"starting trigger_cron: {cron_time}")
+
+    while(trigger:= await get_due_triggers(cron_time)):
+        await call_trigger_graph(trigger)
+        await create_next_triggers(trigger)
